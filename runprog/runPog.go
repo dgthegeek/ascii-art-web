0257a69de20d9e3fba5ef63e3fa26c93e2257e74
Dame Gaye <117014599+dgthegeek@users.noreply.github.com>
2023-04-04 01:05:32 -0700

    Bug fixed

--- runprog/runPog.go
+++ runprog/runPog.go
@@ -9,22 +9,28 @@ import (
 )
 
 func RunProgram(input string, fontFamily string) string {
+	os.Remove("result.txt")
 	var args []string
 	args = append(args, input)
 	args = append(args, selectFile(fontFamily))
 
-	str := input
+		inputTab:= strings.Split(input, "\r\n")
 
-	argsRune := []rune(str)
-	strTab := strings.Split(string(argsRune), `\n`)
-	asciiTable := getAsciiValues(fontFamily)
-	for _, v := range strTab {
-		word := wordFormation([]rune(v), asciiTable)
-		PrintWord(word)
-	}
+		for _,part:= range inputTab{
+			argsRune := []rune(part)
+			strTab := strings.Split(string(argsRune), `\n`)
+			asciiTable := getAsciiValues(fontFamily)
+	
+			for _, v := range strTab {
+				word := wordFormation([]rune(v), asciiTable)
+				PrintWord(word,"result.txt")
+			}	
+		}
+
+		
 	return ReturnFile("result.txt")
-
 }
+
 func ReturnFile(file string) string {
 	data, _ := ioutil.ReadFile(file)
 
@@ -60,48 +66,66 @@ func getAsciiValues(filename string) [][]string {
 }
 
 // This function print the ouput of your treatment
-func PrintWord(words [][]string) {
-	if len(words) == 0 {
-		fmt.Println("test")
-		return
-	}
-	line1 := ""
-	line2 := ""
-	line3 := ""
-	line4 := ""
-	line5 := ""
-	line6 := ""
-	line7 := ""
-	line8 := ""
-	for i := 0; i < len(words); i++ {
-		line1 += words[i][0]
-		line2 += words[i][1]
-		line3 += words[i][2]
-		line4 += words[i][3]
-		line5 += words[i][4]
-		line6 += words[i][5]
-		line7 += words[i][6]
-		line8 += words[i][7]
-	}
-	var filnalString = ""
-	filnalString = concatString(line1, filnalString)
-	filnalString = concatString(line2, filnalString)
-	filnalString = concatString(line3, filnalString)
-	filnalString = concatString(line4, filnalString)
-	filnalString = concatString(line5, filnalString)
-	filnalString = concatString(line6, filnalString)
-	filnalString = concatString(line7, filnalString)
-	filnalString = concatString(line8, filnalString)
 
-	ioutil.WriteFile("result.txt", []byte(filnalString), 0666)
-
+func PrintWord(words [][]string, outputfile string) {
+		if len(words) == 0 {
+			fmt.Println()
+			return
+		}
+		line1 := ""
+		line2 := ""
+		line3 := ""
+		line4 := ""
+		line5 := ""
+		line6 := ""
+		line7 := ""
+		line8 := ""
+	
+		for i := 0; i < len(words); i++ {
+			line1 += words[i][0]
+			line2 += words[i][1]
+			line3 += words[i][2]
+			line4 += words[i][3]
+			line5 += words[i][4]
+			line6 += words[i][5]
+			line7 += words[i][6]
+			line8 += words[i][7]
+		}
+	
+		fileWriter(line1, outputfile)
+		fileWriter(line2, outputfile)
+		fileWriter(line3, outputfile)
+		fileWriter(line4, outputfile)
+		fileWriter(line5, outputfile)
+		fileWriter(line6, outputfile)
+		fileWriter(line7, outputfile)
+		fileWriter(line8, outputfile)
 }
+
 func concatString(s, str string) string {
 
 	str += s + "\n"
 	return str
 
 }
+
+func fileWriter(content string, outputfile string) {
+
+	file, err := os.OpenFile(outputfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(content + "\n")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+
 func selectFile(filename string) string {
 	switch filename {
 	case "thinkertoy":
